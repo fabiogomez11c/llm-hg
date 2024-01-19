20a62250b24a8047080c0592dda84f0fc9d67d50
Fabio Gomez <fabiogomez11c@gmail.com>
2024-01-19 01:48:03 +0000

    temp code

---
+++ utils.py
@@ -0,0 +1,24 @@
+import torch
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+device = "cuda"
+
+model_id = "teknium/OpenHermes-2.5-Mistral-7B"
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+model = AutoModelForCausalLM.from_pretrained(
+    model_id,
+    torch_dtype=torch.float16
+).to(device)
+
+text = """
+You are "Hermes 2", a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia.
+
+<|im_start|>user
+Write a C program to print the input user, be aware that I want to see the complete line that user gives.
+"""
+
+inputs = tokenizer(text, return_tensors="pt").to(device)
+
+outputs = model.generate(**inputs, max_new_tokens=200)
+print(tokenizer.decode(outputs[0], skip_special_tokens=True))
